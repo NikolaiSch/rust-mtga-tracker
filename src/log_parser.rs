e50347566b6d4db7c896775fa03901036ca41fe1
Nikolai <94403162+NikolaiSch@users.noreply.github.com>
2022-10-16 07:46:28 +0100

    getting log parser to work

--- src/log_parser.rs
+++ src/log_parser.rs
@@ -1,36 +1,37 @@
 use std::fs;
-
-pub struct Message {
-    // change this to set values later
-    message_type: String,
-    message_data: String
-}
-
-impl Message {
-    pub fn parse_from_str(data: &str) -> Result<Message, String> {
-        println!("{}", data);
-        todo!()
-    }
-}
+use crate::message_types::start_hook::StartHook;
 
 pub struct Log {
-    pub data: Vec<Message>,
-    pub raw_data: String
+    pub raw_data: String,
+
+    pub profileData: Option<StartHook>
 }
 
 impl Log {
     pub fn new(file_location: &str) -> Log {
-       Log {
-           data: vec![],
-           raw_data: fs::read_to_string(file_location).unwrap()
-       }
+        Log {
+            raw_data: fs::read_to_string(file_location).unwrap(),
+            profileData: None
+        }
     }
 
     pub fn init(&mut self) -> () {
-        let log_presplit = &self.raw_data.replace("[UnityCrossThreadLogger]", "!±");
+        let log_presplit = &self.raw_data.replace("[UnityCrossThreadLogger]", "!");
         let log_vec = log_presplit.split("!");
 
+        log_vec.for_each(|val| {
+            self.parse_from_str(val)
+        })
+    }
 
+    pub fn parse_from_str(&mut self, data: &str) -> () {
+        if data.contains("<== StartHook") {
+            let transformed_data = data
+            .trim()
+            .lines()
+            .nth(2).unwrap();
+
+            self.profileData = Some(StartHook::create_from_str(transformed_data).unwrap());
+        }
     }
 }
-
